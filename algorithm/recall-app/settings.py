9592049d6028385189d597de05be637858059186
minasora <1642940680@qq.com>
2025-05-10 01:42:46 +0900

    fix

--- algorithm/recall-app/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class cfg:
-    faiss_index = "data/recall.index"
-    movie_ids   = "data/movie_ids.npy"
-    user_emb    = "data/user_emb.pt"
--- recall/tests/test_api.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_similar():
-    r = client.get("/similar/80?k=5")
-    assert r.status_code in (200, 404)  # 取决于数据集
